0d8bfc7e7440b69c8c8c284c941bb6b400d4a0c8
Sartaj Singh <sin14136@sheridancollege.ca>
2024-03-29 00:34:57 -0400

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PetsListComponent } from './pets-list/pets-list.component';
+import { PetDetailsComponent } from './pet-details/pet-details.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
+  declarations: [AppComponent, PetsListComponent, PetDetailsComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pets-list/pets-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pets-list',
+  templateUrl: './pets-list.component.html',
+  styleUrl: './pets-list.component.css'
+})
+export class PetsListComponent {
+
+}
